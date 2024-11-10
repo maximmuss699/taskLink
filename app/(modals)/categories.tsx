8b310ecc17483a78fb902b0d5f1e41f8e65eabfa
Vojtech Tichy <tichy.vojtas@gmail.com>
2024-11-10 22:24:32 +0100

    add category buttons

--- app/(modals)/categories.tsx
+++ app/(modals)/categories.tsx
@@ -1,12 +1,95 @@
-import { View, Text} from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Link } from "expo-router";
 import React from 'react';
 
-const Page = () => {
+const Categories = () => {
     return (
         <View>
+            <View>
+            <TouchableOpacity>
+                <Ionicons style={styles.SearchIcon} name='close-outline' size={18}/>
+            </TouchableOpacity>
+
+            <View style={styles.SearchBarCollection}>
+                <Ionicons style={styles.SearchIcon} name='search-outline' size={24}/>
+                <TextInput
+                    style={styles.SearchBar}
+                    placeholder='What job are you searching for?'
+                    placeholderTextColor="black"
+                    />
+            </View>
+
+            <TouchableOpacity>
+                <Ionicons style={styles.SearchIcon} name='options-outline' size={18}/>
+            </TouchableOpacity>
+
             <Text>Categories</Text>
+
+            <Link href={{ pathname: '/(modals)/posts'}}>
+                <Text>Taskers</Text>
+            </Link>
+
+            <TouchableOpacity>
+                <Text>Seekers</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Text>Professionals</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Text>Moving</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Text>Garden</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Text>Furniture</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Text>Housework</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Text>Cleaning</Text>
+            </TouchableOpacity>
+            </View>
+
         </View>
     );
 }
 
-export default Page;
+const styles = StyleSheet.create({
+    SearchBar: {
+        width: 280,
+        margin: 10,
+        marginLeft: 2,
+        padding: 10,
+        alignSelf: 'center',
+        borderWidth: 2,
+        borderColor: '#DEDEDE',
+        borderRadius: 30,
+        backgroundColor: '#FFFFFF',
+        shadowColor: '#DEDEDE',
+        shadowOpacity: 1,
+        shadowOffset: { width: 0, height: 1},
+        flexDirection: 'row',
+        fontFamily: 'mon-b',
+        fontWeight: 'bold'
+    },
+    SearchIcon: {
+        marginLeft: 15,
+        marginTop: 5
+    },
+    SearchBarCollection: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
+
+export default Categories;
