8df73e0bfb1226830ac8cc10217fe51242f7073a
Vojtech Tichy <tichy.vojtas@gmail.com>
2024-11-19 13:39:38 +0100

    add basic add/remove comments

---
+++ app/comments/commentMain.tsx
@@ -0,0 +1,200 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, ViewBase, FlatList, Image, Touchable } from 'react-native';
+import { useLocalSearchParams, router, useRouter } from "expo-router";
+import Colors from "@/constants/Colors";
+import { SearchBar } from 'react-native-screens';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { SafeAreaView } from 'react-native-safe-area-context';
+/* firestore imports */
+import { collection, query, doc, where, onSnapshot, deleteDoc } from 'firebase/firestore';
+import { FIRESTORE } from '@/firebaseConfig';
+
+// evaluation interface
+interface evaluation {
+    id: string; // postId; FIXME: make the variable name unambiguous
+    commId: string;
+    comment: string;
+    rating: number;
+}
+
+// deletes comment with comment id
+async function deleteComment(commId: string) {
+    const docReference = doc(FIRESTORE, 'jobEval', commId);
+    await deleteDoc(docReference);
+}
+
+// computes the average rating
+const computeWholeEval = (evalArr: Array<evaluation>) => {
+    var ratingSum = 0;
+    if (evalArr.length === 0) return 0.0
+    // get the sum
+    evalArr.forEach((arrElem) => {
+        ratingSum += arrElem.rating;
+    })
+    // divide the sum by the number of the elements in the array
+    ratingSum = ratingSum / evalArr.length;
+    return ratingSum.toFixed(1);
+}
+
+// function rendering single comment
+const renderEval = (id: string, rating: number, comment: string, commId: string) => {
+    return(
+        <View style={{ width: "100%" }}>
+        <View style={{ height: 2, backgroundColor: "black", width: "100%", margin: 5, marginBottom: 8, alignSelf: "center" }}></View>
+        <View style={styles.singleComm}>
+            <View style={styles.signleCommL}>
+                <Text style={[styles.evalText, { marginBottom: 8} ]}>USERNAME</Text>
+                <Text style={styles.text}>{comment}</Text>
+            </View>
+            <View style={styles.outerSingleCommR}>
+                <View style={styles.singleCommR}>
+                    <Ionicons name='star' size={15}/>
+                    <Text style={styles.evalText}>{rating}/5</Text>
+                </View>
+                <View style={ { flexDirection: "row", marginTop: 15 } }>
+                    <TouchableOpacity style={styles.commentActionBtn}>
+                        <Ionicons name="pencil-outline" size={25}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.commentActionBtn, { backgroundColor: "#c00" }]} onPress={() => deleteComment(commId)}>
+                        <Ionicons name="trash-outline" size={25}/>
+                    </TouchableOpacity>
+                    {/* FIXME: display prompt when this is clicked */}
+                </View>
+            </View>
+        </View>
+        </View>
+    )
+}
+
+const commentMain = () => {
+
+    const { id } = useLocalSearchParams<{ id: string }>();
+
+    // get the post evaluations
+    const [loadedEvals, setEvals] = useState<evaluation[]>([]);
+    useEffect(() => {
+        const collectionRef = collection(FIRESTORE, "jobEval");
+        const queryQ = query(collectionRef, where('postId', '==', id));
+        const end = onSnapshot(queryQ, (sshot) => {
+            const evalArray: any = [];
+            sshot.docs.forEach((data) => {
+                evalArray.push({ commId: data.id, ...data.data() });
+            })
+            setEvals(evalArray);
+        });
+        return () => end();
+    }, ([]));
+
+    return (
+        <SafeAreaView>
+        <View style={styles.header}>
+            <TouchableOpacity style={styles.backBtn} onPress={() => router.push({pathname: "/"})}>
+                <Ionicons name='chevron-back-outline' size={24}/>
+            </TouchableOpacity>
+            <Text style={styles.mainText}>Job Evaluations</Text>
+        </View>
+        {/* final evaluation */}
+        <View style={styles.evalHeader}>
+            <View style={{ flexDirection: "row", marginLeft: 30 }}>
+                <Ionicons name="star" size={20}/>
+                <Text style={{ fontFamily: 'mon-b', fontSize: 20 }}>{computeWholeEval(loadedEvals)} ({loadedEvals.length})</Text>
+            </View>
+        <TouchableOpacity style={styles.button} onPress={() => router.push({pathname: "/comments/evaluationForm", params: {id} })}>
+            <Text style={styles.buttonText}>Add evaluation</Text>
+        </TouchableOpacity>
+        </View>
+
+        <View style={styles.commentView}>
+            <FlatList
+                data={loadedEvals}
+                renderItem = {({ item }) => renderEval(item.id, item.rating, item.comment, item.commId)}
+                keyExtractor={(item) => item.id}
+            />
+        </View>
+
+    </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    mainText: {
+        fontSize: 28,
+        fontFamily: 'mon-b',
+        fontWeight: 'bold',
+        color: 'black',
+        marginLeft: 5
+    },
+    button: {
+        width: 130,
+        height: 30,
+        backgroundColor: "green",
+        borderRadius: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        margin: 10
+    },
+    buttonText: {
+        color: "white",
+        fontFamily: 'mon-b',
+        fontWeight: 'bold',
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: 14
+    },
+    header: {
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    backBtn: {
+        margin: 20,
+        height: 40,
+        width: 40,
+        backgroundColor: "white",
+        borderRadius: 50,
+        shadowColor: '#DEDEDE',
+        shadowOpacity: 1,
+        shadowOffset: { width: 0, height: 1},
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    commentView: {
+        flexDirection: "column",
+        width: "100%"
+    },
+    singleComm: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 15
+    },
+    signleCommL: {
+        flexDirection: "column",
+        marginLeft: 30,
+    },
+    singleCommR: {
+        flexDirection: "row",
+        marginRight: 30
+    },
+    text: {
+        fontFamily: 'mon'
+    },
+    evalText: {
+        fontFamily: 'mon-sb'
+    },
+    evalHeader: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 30
+    },
+    outerSingleCommR: {
+        flexDirection: "column"
+    },
+    commentActionBtn: {
+        marginRight: 12,
+        borderRadius: 50,
+        backgroundColor: "green",
+        padding: 5
+    }
+})
+
+export default commentMain;
---
+++ app/comments/evaluationForm.tsx
@@ -0,0 +1,186 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { useLocalSearchParams, useRouter } from "expo-router";
+import Colors from "@/constants/Colors";
+import { SearchBar } from 'react-native-screens';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { SafeAreaView } from 'react-native-safe-area-context';
+/* firestore imports */
+import { collection, doc, query, getDocs, addDoc, onSnapshot } from 'firebase/firestore';
+import { FIRESTORE } from '@/firebaseConfig';
+import Slider from '@react-native-community/slider';
+import Toast from 'react-native-toast-message';
+
+// show popup message
+export async function popupMsg() {
+    const showMsg = () => {
+        console.log("Message popup!")
+        Toast.show({
+            type: 'success',
+            text1: 'Your evaluation was successfuly added!'
+        })
+    }
+    showMsg();
+}
+
+// sends the evaluation collected from the user to the firebase
+async function evalToDB(rating: number, comment: string, id: string, router: any) {
+    const collectionRef = collection(FIRESTORE, 'jobEval');
+    await addDoc(collectionRef, { comment: comment, rating: rating, postId: id });
+    popupMsg();
+    setTimeout(() => router.push({pathname: "/comments/commentMain", params: {id}}),1000);
+}
+
+function resizeCommWin() {
+    return styles.Comment.height * 1.5
+}
+
+const evaluationForm = () => {
+    const router = useRouter();
+    const { id } = useLocalSearchParams<{ id: string }>();
+
+    const [sliderVal, setSliderVal] = useState<number>(0);
+    const [comment, setComment] = useState<string>("");
+
+    return (
+    <SafeAreaView style={styles.mainView}>
+        <View style={styles.topScreen}>
+        <TouchableOpacity style={styles.backBtn} onPress={() => router.push({pathname: "/comments/commentMain", params: {id}})}>
+            <Ionicons name='chevron-back-outline' size={24}/>
+        </TouchableOpacity>
+        <Text style={styles.mainText}>Evaluate Tasker</Text>
+        </View>
+        {/* final evaluation */}
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View style={styles.header}>
+            <View style={styles.rating}>
+                <Ionicons name="star" size={25}/>
+                <Text style={styles.ratingText}> {sliderVal}</Text>
+            </View>
+            <Slider
+                style={styles.evalSlider}
+                minimumValue={0}
+                maximumValue={5}
+                step={1}
+                maximumTrackTintColor="#000000"
+                minimumTrackTintColor="#FFFFFF"
+                onValueChange={(value) => {setSliderVal(value)}}
+            />
+                <TextInput
+                    style={styles.Comment}
+                    placeholder='Add your comment here'
+                    placeholderTextColor="gray"
+                    onChangeText={(comm) => setComment(comm)}
+                    value={comment}
+                    multiline
+                    numberOfLines={8}
+                    onContentSizeChange={resizeCommWin}
+                />
+        </View>
+        </TouchableWithoutFeedback>
+
+        <TouchableOpacity style={styles.button} onPress={() => evalToDB(sliderVal, comment, id, router)}>
+            <Text style={styles.buttonText}>Add evaluation</Text>
+        </TouchableOpacity>
+    </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    mainView: {
+        flexDirection: "column"
+    },
+    mainText: {
+        fontSize: 28,
+        fontFamily: 'mon-b',
+        fontWeight: 'bold',
+        color: 'black',
+        marginLeft: 15
+    },
+    button: {
+        marginTop: 300,
+        alignSelf: "center",
+        width: 200,
+        height: 50,
+        backgroundColor: "green",
+        borderRadius: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        margin: 10
+    },
+    buttonText: {
+        color: "white",
+        fontFamily: 'mon-b',
+        fontWeight: 'bold',
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: 14
+    },
+    header: {
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    form: {
+        width: "90%",
+        height: 50,
+        shadowColor: '#DEDEDE',
+        shadowOpacity: 1,
+        shadowOffset: { width: 0, height: 1},
+    },
+    Comment: {
+        marginTop: 30,
+        width: 280,
+        height: 120,
+        margin: 10,
+        marginLeft: 2,
+        padding: 10,
+        alignSelf: 'center',
+        borderWidth: 2,
+        borderColor: '#DEDEDE',
+        borderRadius: 10,
+        backgroundColor: '#FFFFFF',
+        shadowColor: '#DEDEDE',
+        shadowOpacity: 1,
+        shadowOffset: { width: 0, height: 1},
+        flexDirection: 'row',
+        fontFamily: 'mon',
+        fontWeight: 'bold'
+    },
+    evalSlider: {
+        marginTop: 20,
+        marginBottom: 40,
+        width: 250,
+    },
+    ratingText: {
+        marginTop: 2,
+        fontFamily: 'mon-b',
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: 20
+    },
+    rating: {
+        marginTop: 20,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    backBtn: {
+        margin: 20,
+        height: 40,
+        width: 40,
+        backgroundColor: "white",
+        borderRadius: 50,
+        shadowColor: '#DEDEDE',
+        shadowOpacity: 1,
+        shadowOffset: { width: 0, height: 1},
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    topScreen: {
+        flexDirection: "row",
+        alignItems: "center"
+    }
+})
+
+export default evaluationForm;
