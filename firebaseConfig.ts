a8094191fad418c37c5b3cf6bebbc2e74e9e5019
Max Samusevich <93098818+maximmuss699@users.noreply.github.com>
2025-03-02 21:15:22 +0100

    Regenerated API key

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -10,13 +10,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAkA41dS2MO7WWG4tKlOmN1GMEoKj59sAk",
-    authDomain: "tasklink-4a1d5.firebaseapp.com",
-    projectId: "tasklink-4a1d5",
-    storageBucket: "tasklink-4a1d5.firebasestorage.app",
-    messagingSenderId: "954364379029",
-    appId: "1:954364379029:web:4650f1a0bef1c90b81c09c",
-    measurementId: "G-8QYDHH8Y2F"
+    apiKey: "", // key is obsolete
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
